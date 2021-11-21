27e69ee7c1b3d5bd4e3482b08ddf530d809e7a0c
g.remigi <g.remigi@kineticsystem.org>
2021-11-21 15:12:21 +0100

    Paho running correctly.

--- main.cpp
+++ main.cpp
@@ -4,21 +4,65 @@
 // Unauthorized copying of this code base via any medium is strictly prohibited.
 // Proprietary and confidential.
 
+#include "mqtt/async_client.h"
 #include "proto/sensor.pb.h"
+
 #include <fstream>
 #include <iostream>
 
+namespace {
+
+constexpr auto DFLT_SERVER_ADDRESS = "tcp://localhost:1883";
+constexpr auto CLIENT_ID = "paho_cpp_async_publish";
+
+constexpr auto TOPIC = "test";
+constexpr int QOS = 1;
+constexpr auto LWT_PAYLOAD = "Last will and testament.";
+constexpr auto TIMEOUT = std::chrono::seconds(10);
+
+constexpr auto PAYLOAD = "Hello World!";
+
+} // namespace
+
 int main() {
 
-  Sensor sensor;
-  sensor.set_name("Laboratory");
-  sensor.set_temperature(23.4);
-  sensor.set_humidity(68);
-  sensor.set_door(Sensor_SwitchLevel_OPEN);
+  //  Sensor sensor;
+  //  sensor.set_name("Laboratory");
+  //  sensor.set_temperature(23.4);
+  //  sensor.set_humidity(68);
+  //  sensor.set_door(Sensor_SwitchLevel_OPEN);
 
-  std::ofstream ofs("sensor.data", std::ios_base::out | std::ios_base::binary);
-  sensor.SerializeToOstream(&ofs);
+  //  std::ofstream ofs("sensor.data", std::ios_base::out |
+  //  std::ios_base::binary); sensor.SerializeToOstream(&ofs);
 
-  std::cout << "Hello Proto!" << std::endl;
+  mqtt::async_client client(DFLT_SERVER_ADDRESS, CLIENT_ID);
+
+  auto connection_opts = mqtt::connect_options_builder()
+                             .clean_session()
+                             .will(mqtt::message(TOPIC, LWT_PAYLOAD, QOS))
+                             .finalize();
+
+  try {
+
+    // Connect.
+
+    mqtt::token_ptr token = client.connect(connection_opts);
+    token->wait();
+
+    // Send a message.
+
+    mqtt::message_ptr pubmsg = mqtt::make_message(TOPIC, PAYLOAD);
+    pubmsg->set_qos(QOS);
+    client.publish(pubmsg)->wait_for(TIMEOUT);
+
+    // Disconnect.
+
+    client.disconnect()->wait();
+
+  } catch (const mqtt::exception &exc) {
+    std::cerr << exc.what() << std::endl;
+    return 1;
+  }
+
   return 0;
 }
